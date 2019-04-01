02014e867da15258b455b6d775abef11f52219ef
Adam Harmanec <adam@harmanec.com>
2019-04-01 12:46:24 +0200

    third task

--- assignment6.py
+++ assignment6.py
@@ -1,5 +1,7 @@
 from functools import reduce
 from operator import add
+import random as rnd
+import string
 
 
 def my_sum(*args):
@@ -16,10 +18,30 @@ def is_palindrome(word):
     return True
 
 
-if __name__ == '__main__': # test the implementations
+def generate_password(length=8, special_chars_num=2):
+    if special_chars_num > length:
+        raise ValueError
+
+    special_chars = ['@', '?', '$', '#', '%', '&']
+    special_chars_indexes = rnd.sample(list(range(length)), special_chars_num)
+
+    password = ''
+    for i in range(length):
+        if i in special_chars_indexes:
+            password += rnd.choice(special_chars)
+        else:
+            password += rnd.choice(string.ascii_letters)
+
+    return password
+
+
+if __name__ == '__main__':  # test the implementations
     print(my_sum(1, 2, 3))
     print(my_sum(-5, 4, 18, -25))
 
     print(is_palindrome('kajak'))
     print(is_palindrome('okko'))
     print(is_palindrome('not a palindrome'))
+
+    print(generate_password())
+    print(generate_password(length=20, special_chars_num=5))
