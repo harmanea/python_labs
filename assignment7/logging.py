cf3af7acef38b36a16bd87eb9cf64fc48984375e
Adam Harmanec <adam@harmanec.com>
2019-04-08 13:26:46 +0200

    added Formatters

--- assignment7/logging.py
+++ assignment7/logging.py
@@ -1,4 +1,5 @@
 import sys
+import datetime
 
 
 class Logger:
@@ -13,18 +14,30 @@ class Logger:
     def log(self, level: int, message: str):
         if level > self.level:
             for printer in self.printers:
-                printer.print(message)
+                printer.print(message, self)
 
     def add_printer(self, printer):
         self.printers.append(printer)
 
 
 class Printer:
+    def __init__(self, fun=lambda msg: print(msg), formatter=None):
+        self.fun = fun
+        self.formatter = formatter
+
+    def print(self, msg, logger):
+        if self.formatter is None:
+            self.fun(msg)
+        else:
+            self.fun(self.formatter.format(logger, msg))
+
+
+class Formatter:
     def __init__(self, fun):
         self.fun = fun
 
-    def print(self, msg):
-        self.fun(msg)
+    def format(self, logger, message):
+        return self.fun(logger, message)
 
 
 if __name__ == '__main__':  # test the implementation
@@ -43,3 +56,19 @@ if __name__ == '__main__':  # test the implementation
     log.set_level(1)
 
     log.log(2, 'now it\'s okay')
+
+    formatter = Formatter(lambda logger, msg: f'{logger.name}: {msg}')
+    now_formatter = Formatter(lambda logger, msg: f'{datetime.datetime.now()}: {msg}')
+
+    print(formatter.format(log, 'A cool msg'))
+    print(now_formatter.format(log, 'Another cool msg'))
+
+    log = Logger('Logger with formatters')
+
+    printer = Printer(lambda msg: print(msg), formatter)
+    other_printer = Printer(lambda msg: print(msg), now_formatter)
+
+    log.add_printer(printer)
+    log.add_printer(other_printer)
+
+    log.log(2, 'Message with formatters')
