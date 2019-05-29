a0e8a9a70dc8a954b6562b51ab41f18373d5751c
Adam Harmanec <adam.harmanec@volny.cz>
2019-05-29 22:53:40 +0200

    Strukt now works

--- assignment11/strukt.py
+++ assignment11/strukt.py
@@ -1,22 +1,25 @@
 import collections
 
-class Strukt(type):
-    @classmethod
-    def __prepare__(mcs, name, bases, **kwargs):
-        print('prep', name, bases, **kwargs)
-        return kwargs
 
-    def __new__(mcs, **kwargs):
-        print('new', kwargs)
-        return super(Strukt, mcs).__new__(mcs, 'new_name', tuple(), kwargs)
+class metaStrukt(type):
+    def __new__(mcs, name, bases, namespace):
+        namespace['__name__'] = name
+        namespace['__slots__'] = [key for key in namespace.keys() if not key.startswith('__')]
+        namespace = {key: namespace[key] for key in namespace.keys() if key.startswith('__')}
+        return super().__new__(mcs, name, bases, namespace)
 
+    def __init__(cls, name, bases, namespace):
+        super().__init__(name, bases, namespace)
+
+
+class Strukt(metaclass=metaStrukt):
     def __init__(self, **kwargs):
-        print('init', kwargs)
-        super(Strukt, self).__init__(self.__name__, tuple(), {})
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def __repr__(self):
-        print('repr')
-        return self.__name__ + '(' + self.__slots__ + ")"
+        pairs = [key + '=' + str(getattr(self, key)) for key in self.__slots__ if getattr(self, key, None) is not None]
+        return self.__name__ + '(' + ', '.join(pairs) + ')'
 
 
 class Point(Strukt):
@@ -25,7 +28,7 @@ class Point(Strukt):
 
 
 if __name__ == '__main__':
-    p = Point(y = 8)
+    p = Point(y=8)
     print(p)  # Point(y=8)
     p.y = 2
     p.x = 3
